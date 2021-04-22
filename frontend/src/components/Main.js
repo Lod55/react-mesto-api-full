5c19d2983a7b39c77536350127cebfe99104a29a
Artyom Lodochnikov <lodkin55@yandex.ru>
2021-04-22 12:36:10 +0500

    feat: reverse cards

--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -138,7 +138,7 @@ const Main = (props) => {
                 onCardTrash={handleTrashClick}
               />
             )
-            )}
+            ).reverse()}
           </section>)
         }
       </main>
